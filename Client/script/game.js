c316c7b90e5fcf9de9ec3df43ee8c8afc4a571c9
ThePizzaguy <oscar.van.zanten@windesheim.nl>
2016-03-23 14:40:28 +0100

    Server browser work until now

--- Client/script/game.js
+++ Client/script/game.js
@@ -3,7 +3,89 @@ socket = io.connect("http://localhost:8000", {query: "name="+ $("#name").val()})
 keyinputs = [false, false];
 var canvas = document.getElementById("canvas");
 var graphics = canvas.getContext("2d");
+var width = $("#canvas").width();
+var height = $("#canvas").height();
+var splasscreen =   new Image();
+splasscreen.src="img/Splasscreen.png"
+
+var status = "Startup";
+
 var lobby;
+
+//splasscreen
+$("#canvas").ready(function(){
+	graphics.drawImage(splasscreen,0,0,width ,height);
+	setTimeout(function(){
+		status  = "Browser";
+		graphics.beginPath();		
+		graphics.fillStyle = "#FFFFFF";
+		graphics.fillRect(0,0, width,height);
+		socket.emit("serverListRefresh", []);
+	}, 2000); 
+});
+
+//draw game if lobby is set
+setInterval(function() {
+	if(status == "Browser"){
+	
+	}
+	
+	if(status == "Ingame"){
+		if( lobby === undefined){
+			return;
+		}
+		graphics.beginPath();		
+		graphics.fillStyle = "#FFFFFF";
+		graphics.fillRect(0,0, width,height);
+		var center= new point(width/2,height/2);
+		var radius = 280;
+		var points = [];
+		var count= lobby.currentlyplaying;
+		if(count < 4)
+			count = 4;
+		for(var i = 0 ; i < count;i++){
+			var angle = Math.PI * 2 / count * i + (1/4 *Math.PI) ;
+			var x =Math.cos(angle) *radius + center.x;
+			var y =Math.sin(angle) *radius + center.y;
+			points.push(new point(x,y));
+		}
+		for(var i = 1 ; i < points.length;i++){
+			graphics.moveTo(points[i-1].x,points[i-1].y);
+			graphics.lineTo(points[i].x,points[i].y);
+			graphics.stroke();
+		}
+		graphics.moveTo(points[0].x,points[0].y);
+		graphics.lineTo(points[points.length-1].x,points[points.length-1].y);
+		graphics.stroke();
+		for(var i = 0 ; i < lobby.users.length;i++){
+			var angle = Math.PI * 1/2 * i + (1/4 *Math.PI);
+			var barlength = ( Math.sin((2 *Math.PI / count)/2)*radius*2) ;
+			var x = 0;
+			var y = 0;
+			if(i % 2){
+				x = Math.cos(angle) *radius + center.x + (Math.sin(45*Math.PI/180 + angle -  (1/2 *Math.PI)	) *barlength / 100 * lobby.users[i].location);
+				y = Math.sin(angle) *radius + center.y + (Math.cos(45*Math.PI/180 + angle -  (1/2 *Math.PI)) *barlength / 100 * lobby.users[i].location);
+			} else{
+				x = Math.cos(angle) *radius + center.x - (Math.sin(45*Math.PI/180 + angle -  (1/2 *Math.PI)) *barlength / 100 * lobby.users[i].location);
+				y = Math.sin(angle) *radius + center.y - (Math.cos(45*Math.PI/180 + angle -  (1/2 *Math.PI)) *barlength / 100 * lobby.users[i].location);
+			}
+			graphics.beginPath();		
+			graphics.translate( x, y );
+			graphics.rotate(angle+  (1	/4 *Math.PI));
+			graphics.rect(-100/2,-10/2, 100,10);	
+			graphics.fillStyle = "#FF0000";
+			graphics.fill();
+			graphics.setTransform(1, 0, 0, 1, 0, 0);
+		}
+	}
+}, 1000/30);
+
+socket.on("gameupdate", function (data) {
+	lobby = data;
+});
+socket.on("lobbies", function (data) {
+	lobbies = data;
+});
 
 //name change
 $("#name").change(function(){
@@ -40,67 +122,6 @@ window.addEventListener('keyup', function(event) {
 setInterval(function (){
 	socket.emit("updateKeys", keyinputs);
 } , 1000/20);
-
-setInterval(function() {
-	graphics.beginPath();		
-	graphics.fillStyle = "#FFFFFF";
-	graphics.fillRect(0,0, 600,600);
-	var center= new point(300,300);
-	var radius = 280;
-	var points = [];
-	var count= lobby.currentlyplaying;
-	if(count < 4)
-		count = 4;
-
-	for(var i = 0 ; i < count;i++){
-		var angle = Math.PI * 2 / count * i + (1/4 *Math.PI) ;
-		var x =Math.cos(angle) *radius + center.x;
-		var y =Math.sin(angle) *radius + center.y;
-		points.push(new point(x,y));
-	}
-	
-	for(var i = 1 ; i < points.length;i++){
-		graphics.moveTo(points[i-1].x,points[i-1].y);
-		graphics.lineTo(points[i].x,points[i].y);
-		graphics.stroke();
-	}
-	
-	graphics.moveTo(points[0].x,points[0].y);
-	graphics.lineTo(points[points.length-1].x,points[points.length-1].y);
-	graphics.stroke();
-	
-	for(var i = 0 ; i < lobby.users.length;i++){
-		var angle = Math.PI * 1/2 * i + (1/4 *Math.PI);
-		var barlength = ( Math.sin((2 *Math.PI / count)/2)*radius*2) ;
-		
-		var x = 0;
-		var y = 0;
-		
-		if(i % 2){
-			x = Math.cos(angle) *radius + center.x + (Math.sin(45*Math.PI/180 + angle -  (1/2 *Math.PI)	) *barlength / 100 * lobby.users[i].location);
-			y = Math.sin(angle) *radius + center.y + (Math.cos(45*Math.PI/180 + angle -  (1/2 *Math.PI)) *barlength / 100 * lobby.users[i].location);
-		} else{
-			x = Math.cos(angle) *radius + center.x - (Math.sin(45*Math.PI/180 + angle -  (1/2 *Math.PI)) *barlength / 100 * lobby.users[i].location);
-			y = Math.sin(angle) *radius + center.y - (Math.cos(45*Math.PI/180 + angle -  (1/2 *Math.PI)) *barlength / 100 * lobby.users[i].location);
-		}
-		
-		graphics.beginPath();		
-		
-		graphics.translate( x, y );
-		graphics.rotate(angle+  (1	/4 *Math.PI));
-		
-		graphics.rect(-100/2,-10/2, 100,10);	
-		graphics.fillStyle = "#FF0000";
-		graphics.fill();
-		
-		graphics.setTransform(1, 0, 0, 1, 0, 0);
-	}
-}, 1000/30);
-
-socket.on("gameupdate", function (data) {
-	lobby = data;
-});
-
 
 function point(x,y){
 	this.x = x;
--- Server/server.js
+++ Server/server.js
@@ -3,57 +3,50 @@ var http = require('http');
 var server = http.createServer( handler );
 var io = require("socket.io").listen(server);
 server.listen(8000);
-console.log("listening on port 8000");
 
-var lobby = new lobby(2);
-
-function handler( request, response ) {
-	response.writeHead(200 , { "Content-Type": "text/plain"});
- 	response.write("Nothing here to find.... <br><br><br> Why are you here?...");
-    response.end();
-    console.log("response sent..");
-};
+var lobbies = [];
+var lobby = new lobby("Main Lobby", 128, 0);
 
 io.sockets.on("connection", function(socket) {
 	var data = socket.handshake.query;
-	console.log("user connected: '" + data.name +"'" );
+	console.log("user: '" + data.name +"' connected to the main Lobby" );
 	lobby.users.push(new user(data.name, socket.id));
-	
-	
 	socket.on("namechange", function (data) {
-		var id = getIndexForUser(lobby.users, socket.id);
-		console.log("User '" + lobby.users[id].name + "' changed his name to '" + data + "'");
-		lobby.users[id].name = data;
+		var room = getLobbyForUser(socket.id);
+		var id = getIndexForUser(room.users, socket.id);
+		console.log("User '" + room.users[id].name + "' changed their name to '" + data + "'");
+		room.users[id].name = data;
 	});
-	
 	socket.on("updateKeys", function (data) {
-		var id = getIndexForUser(lobby.users, socket.id);
+		var room = getLobbyForUser(socket.id);
+		var id = getIndexForUser(room.users, socket.id);
 		for(var i =0; i < data.length;i++){
-			lobby.users[id].keys[i] = data[i];
+			room.users[id].keys[i] = data[i];
 		}
 	});
-	
 	socket.on('disconnect', function() {
-		var id = getIndexForUser(lobby.users, socket.id);
-		console.log("user disconnected: '" +  lobby.users[id].name+"'");
-		lobby.users.splice(id,1);
+		var room = getLobbyForUser(socket.id);
+		var id = getIndexForUser(room.users, socket.id);
+		console.log("user disconnected: '" +  room.users[id].name+"'");
+		room.users.splice(id,1);
+	});
+	socket.on('serverListRefresh', function() {
+		io.sockets.connected[socket.id].emit('lobbies', lobbies);
 	});
 });
 
 
 setInterval(function() {
 	console.log(lobby);
+	console.log(lobbies);
 }, 5000);
 
 setInterval(function() {
-	update();
-	io.sockets.emit("gameupdate",lobby);
+	for(var i = 0; i < lobbies.length; i ++){
+		lobbies[i].update();
+	}
+	//io.sockets.emit("gameupdate",lobby);
 }, 1000/60);
-
-
-function update(){
-	lobby.update();
-}
 
 function user(name, id){
 	this.name = name;
@@ -75,17 +68,35 @@ function ball(xAs, yAs){
 	this.angle = 0;
 }
 
-function lobby( maxplayers){
+function lobby(name, maxplayers, id){
+	this.id = id;
+	this.name = name;
 	this.maxplayers = maxplayers
 	this.users = [];
-	this.currentlyplaying = 2;
+	this.currentlyplaying =0;
 	this.ball = ball;
-	
 	this.update = function update(){
+		currentlyplaying = users.length;
 		for(var i =0; i < this.users.length; i++){
 			this.users[i].update();
 		}
 	}
+}
+
+function getLobbyForUser(id){
+	for(var i =0; i < lobby.users.length;i++){
+		if(lobby.users[i].id ==id){
+			return lobby
+		}
+	}
+	for(var i =0; i < lobbies.length;i++){
+		for(var x = 0; x< lobbies[i].users.length; x++){
+			if(lobbies[i].users[x].id == id){
+				return lobbies[i];
+			}
+		}
+	}
+	return -1;
 }
 
 function getIndexForUser(users, id){
@@ -96,3 +107,10 @@ function getIndexForUser(users, id){
 	}
 	return -1;
 }
+
+function handler( request, response ) {
+	response.writeHead(200 , { "Content-Type": "text/plain"});
+ 	response.write("Nothing here to find.... <br><br><br> Why are you here?...");
+    response.end();
+    console.log("response sent..");
+};
